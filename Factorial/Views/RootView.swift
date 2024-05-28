cc9303b8eb0ed5ec096065c7cd36bd91af1c357c
T1m00fey <forG1tHub@yandex.ru>
2024-05-28 13:19:55 +0400

    upds

---
+++ Factorial/Views/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  Factorial
+//
+//  Created by Тимофей Юдин on 24.05.2024.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
